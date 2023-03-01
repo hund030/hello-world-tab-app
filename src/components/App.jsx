717248fdf41550dc9ee5644b29beafc07f4f0c31
zhijie <zhihuan@microsoft.com>
2023-03-01 14:20:57 +0800

    feat: remove teamsfx sdk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import Privacy from "./Privacy";
 import TermsOfUse from "./TermsOfUse";
 import Tab from "./Tab";
 import TabConfig from "./TabConfig";
-import { TeamsFxContextProvider } from "./Context";
 
 /**
  * The main app which handles the initialization and routing
@@ -12,16 +11,14 @@ import { TeamsFxContextProvider } from "./Context";
  */
 export default function App() {
   return (
-    <TeamsFxContextProvider>
-      <Router>
-        <Route exact path="/">
-          <Redirect to="/tab" />
-        </Route>
-        <Route exact path="/privacy" component={Privacy} />
-        <Route exact path="/termsofuse" component={TermsOfUse} />
-        <Route exact path="/tab" component={Tab} />
-        <Route exact path="/config" component={TabConfig} />
-      </Router>
-    </TeamsFxContextProvider>
+    <Router>
+      <Route exact path="/">
+        <Redirect to="/tab" />
+      </Route>
+      <Route exact path="/privacy" component={Privacy} />
+      <Route exact path="/termsofuse" component={TermsOfUse} />
+      <Route exact path="/tab" component={Tab} />
+      <Route exact path="/config" component={TabConfig} />
+    </Router>
   );
 }
--- src/components/samples/sample.jsx
+++ src/components/samples/sample.jsx
@@ -1,21 +1,8 @@
-import { useTeamsFxContext } from "../Context";
-import { useData } from "@microsoft/teamsfx-react";
-import { UserProfile } from "./graph";
-
 export const Sample = () => {
-  const teamsUserCredential = useTeamsFxContext();
-  const { loading, data, error } = useData(async () => {
-    const userInfo = await teamsUserCredential.getUserInfo();
-    return userInfo;
-  });
-
   return (
     <div>
       <h2>Hello World</h2>
-      {loading && "loading"}
-      {!loading && !!data && !error && "Congratulation " + data.displayName + "! Your app is running well."}
-      {!loading && !!error && error.toString()}
-      {UserProfile()}
+      Congratulation! Your app is running well.
     </div>
   );
 };
