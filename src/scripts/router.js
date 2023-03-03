eb7b7ccc79ed7e42307e6557f805d0a46590df68
zhijie <zhihuan@microsoft.com>
2023-03-03 14:34:55 +0800

    chore: config page do not work, remove it

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -20,25 +20,10 @@ function privacyHandler() {
   root_div.appendChild(div);
 }
 
-function configHandler() {
-  removeChildren(root_div);
-  console.log("config");
-}
-
-function indexHandler() {
-  const div = document.createElement("div");
-  div.innerHTML = "<h1>Hello World</h1> <p>Your app is running well.</p>";
-  removeChildren(root_div);
-  root_div.appendChild(div);
-}
-
 function route() {
   const hash = location.hash.slice(1).toLocaleLowerCase() || "/";
   hash === "/termofuse" && termOfUseHandler();
   hash === "/privacy" && privacyHandler();
-  hash === "/config" && configHandler();
-  hash === "/" && indexHandler();
 }
 
-window.addEventListener("hashchange", route);
 window.addEventListener("load", route);
