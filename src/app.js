5a7170a8fc2e8c38d330514f680aba4c579def31
zhijie <zhihuan@microsoft.com>
2023-03-03 16:23:27 +0800

    chore: failed to call graph api - invalid audience

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const restify = require("restify");
 const send = require("send");
 const fs = require("fs");
+const { Client } = require("@microsoft/microsoft-graph-client");
+require('isomorphic-fetch');
 
 //Create HTTP server.
 const server = restify.createServer({
@@ -13,12 +15,7 @@ const server = restify.createServer({
   },
 });
 
-server.get(
-  "/*",
-  restify.plugins.serveStatic({
-    directory: __dirname + "/static",
-  })
-);
+server.use(restify.plugins.bodyParser());
 
 server.listen(process.env.port || process.env.PORT || 3333, function () {
   console.log(`\n${server.name} listening to ${server.url}`);
@@ -33,4 +30,31 @@ server.get("/", (req, res, next) => {
 // Setup the static tab
 server.get("/tab", (req, res, next) => {
   send(req, "src/views/hello.html").pipe(res);
+});
+
+server.get("/auth-start", (req, res, next) => {
+  send(req, "src/views/auth-start.html").pipe(res);
+});
+
+server.get("/auth-end.html", (req, res, next) => {
+  send(req, "src/views/auth-end.html").pipe(res);
+});
+
+server.post("/getUserInfo", async (req, res) => {
+  console.log(req.body);
+  const accessToken = req.body.token;
+  console.log(accessToken);
+  try {
+    const client = Client.init({
+      authProvider: (done) => {
+        done(null, accessToken.trim());
+      },
+    });
+    const result = await client.api("/me").get();
+    res.json(result);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
 });
