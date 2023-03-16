f9409de76d01973de790afcb90350dd989826aa1
zhijie <zhihuan@microsoft.com>
2023-03-16 17:00:24 +0800

    feat: setup m365 infra

--- src/static/scripts/teamsapp.js
+++ src/static/scripts/teamsapp.js
@@ -5,9 +5,12 @@
   microsoftTeams.app.initialize();
 
   // Check the initial theme user chose and respect it
-  microsoftTeams.app.getContext(function (context) {
+  microsoftTeams.app.getContext().then(function (context) {
     if (context && context.theme) {
       setTheme(context.theme);
+    }
+    if (context?.app?.host?.name) {
+      updateHubName(context.app.host.name);
     }
   });
 
@@ -19,9 +22,16 @@
   // Set the desired theme
   function setTheme(theme) {
     if (theme) {
+      console.log(theme);
       // Possible values for theme: 'default', 'light', 'dark' and 'contrast'
       document.body.className =
         "theme-" + (theme === "default" ? "light" : theme);
+    }
+  }
+
+  function updateHubName(hubName) {
+    if (hubName) {
+      document.getElementById("hubName").innerHTML = hubName;
     }
   }
 })();
