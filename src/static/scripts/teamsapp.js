1f60a85d1b3217985819e021a4502c115734f259
zhijie <zhihuan@microsoft.com>
2023-03-21 15:15:34 +0800

    style: update css

--- src/static/scripts/teamsapp.js
+++ src/static/scripts/teamsapp.js
@@ -4,30 +4,11 @@
   // Call the initialize API first
   microsoftTeams.app.initialize();
 
-  // Check the initial theme user chose and respect it
   microsoftTeams.app.getContext().then(function (context) {
-    if (context && context.theme) {
-      setTheme(context.theme);
-    }
     if (context?.app?.host?.name) {
       updateHubName(context.app.host.name);
     }
   });
-
-  // Handle theme changes
-  microsoftTeams.app.registerOnThemeChangeHandler(function (theme) {
-    setTheme(theme);
-  });
-
-  // Set the desired theme
-  function setTheme(theme) {
-    if (theme) {
-      console.log(theme);
-      // Possible values for theme: 'default', 'light', 'dark' and 'contrast'
-      document.body.className =
-        "theme-" + (theme === "default" ? "light" : theme);
-    }
-  }
 
   function updateHubName(hubName) {
     if (hubName) {
